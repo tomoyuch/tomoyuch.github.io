bb016d00b5c20cfe399ab41041b3392a9ebf24a6
Tomoyuki <tomoyuch@gmail.com>
2020-06-20 19:32:39 +0100

    third commit

---
+++ build/precache-manifest.6ddce101c890835714fdd26f69744176.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5dec517abdb97d719793f3c40ea319f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f76a373983b6517dc1",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1590f1c3f4f59cfd92ca",
+    "url": "/static/css/main.afac714a.chunk.css"
+  },
+  {
+    "revision": "d4f76a373983b6517dc1",
+    "url": "/static/js/2.f6e16597.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.f6e16597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1590f1c3f4f59cfd92ca",
+    "url": "/static/js/main.779caedd.chunk.js"
+  },
+  {
+    "revision": "1fcd538383057d714661",
+    "url": "/static/js/runtime-main.8d10fc29.js"
+  }
+]);
